aea1e2ec9461bdb839c9a2b5d4f4c1e5f16e7aca
3 <e2>
2022-03-23 12:18:42 +0530

    First commit

---
+++ app/src/main/java/com/example/resumemaker/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.resumemaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private SeekBar seekBar;
+    private TextView textView1;
+    private SeekBar seekBar1;
+    private TextView textView2;
+    private SeekBar seekBar2;
+    private TextView textView3;
+    private SeekBar seekBar3;
+    private TextView textView4;
+    private SeekBar seekBar4;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.txtguj);
+        seekBar = (SeekBar) findViewById(R.id.sekguj);
+        textView1 = (TextView) findViewById(R.id.txthin);
+        seekBar1 = (SeekBar) findViewById(R.id.sekhin);
+        textView2 = (TextView) findViewById(R.id.txteng);
+        seekBar2 = (SeekBar) findViewById(R.id.sekeng);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textView.setText(""+i+"%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textView2.setText(""+i+"%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textView1.setText(""+i+"%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+}
